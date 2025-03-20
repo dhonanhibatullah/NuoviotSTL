c4d04edf1189f7fd29bab50dfeb3e41d9da655b7
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2025-03-20 15:52:09 +0700

    feat!: new example on NVList

--- examples/NVList/Methods/Methods.ino
+++ examples/NVList/Methods/Methods.ino
@@ -10,53 +10,54 @@ void setup()
 void loop()
 {
     // Create an NVList instance
-    NVList<int> myList = NVList<int>();
+    NVList myList = NVList();
 
     // Append some entries
-    myList.append(1);
-    myList.append(2);
-    myList.append(3);
+    myList.append(123U);
+    myList.append(3.14159F);
+    myList.append("Hello, World!");
 
-    Serial.println("1st action:");
-    for (uint16_t i = 0; i < myList.length(); ++i)
-    {
-        Serial.printf("%d ", myList.get(i));
-    }
-    Serial.println('\n');
+    // Print the list
+    Serial.println("[1] Initializing NVList");
+    Serial.println(myList[0].asUInt32());
+    Serial.println(myList[1].asFloat());
+    Serial.println(myList[2].asString());
+    Serial.println();
 
     // Insert entries
-    myList.insert(2, 39); // 1, 2, 39, 3
-    myList.insert(1, 56); // 1, 56, 2, 39, 3
+    myList.insert(2, "John Doe"); // 123, 3.14159, John Doe, Hello, World!
+    myList.insert(0, 999U);       // 999, 123, 3.14159, John Doe, Hello, World!
 
-    Serial.println("2nd action:");
-    for (uint16_t i = 0; i < myList.length(); ++i)
-    {
-        Serial.printf("%d ", myList.get(i));
-    }
-    Serial.println('\n');
+    // Print the list
+    Serial.println("[2] Inserting new entries");
+    Serial.println(myList[0].asUInt32());
+    Serial.println(myList[1].asInt32());
+    Serial.println(myList[2].asFloat());
+    Serial.println(myList[3].asString());
+    Serial.println(myList[4].asString());
+    Serial.println();
 
-    // Change entries
-    myList.set(1, 100); // 1, 100, 2, 39, 3
-    myList.set(2, 999); // 1, 100, 999, 39, 3
+    // Change entries, can change type
+    myList[0] = "My";
+    myList[1] = "Name";
+    myList[2] = "Is";
 
-    Serial.println("3rd action:");
-    for (uint16_t i = 0; i < myList.length(); ++i)
-    {
-        Serial.printf("%d ", myList.get(i));
-    }
-    Serial.println('\n');
+    // Print the list
+    Serial.println("[3] Changiing entries");
+    Serial.println(myList[0].asString());
+    Serial.println(myList[1].asString());
+    Serial.println(myList[2].asString());
+    Serial.println(myList[3].asString());
+    Serial.println(myList[4].asString());
+    Serial.println();
 
     // Pop entries
-    int a = myList.pop(0); // 100, 999, 39, 3
-    int b = myList.pop(3); // 100, 999, 39
-
-    Serial.println("4th action:");
-    Serial.printf("POPPED: %d %d\nREMAIN: ", a, b);
-    for (uint16_t i = 0; i < myList.length(); ++i)
+    Serial.println("[4] Popping entries");
+    while (myList.length() > 0)
     {
-        Serial.printf("%d ", myList.get(i));
+        Serial.println(myList.pop(0).asString());
     }
-    Serial.println('\n');
+    Serial.println();
 
     // Halt!
     Serial.println("_________[ DONE! ]_________");
